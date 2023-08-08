d71409a1497eb4afa85612040598ec24b0359b9c
chlcksgh <jerryyaho12@daum.net>
2023-08-08 19:27:11 +0900

    Basic-Python study

---
+++ Basic-Python/if-elif-else.py
@@ -0,0 +1,12 @@
+math = int(input("수학 점수를 입력해주세요. : "))
+
+if math >= 90:
+    print("A")
+elif math >= 80:
+    print("B")
+elif math >= 70:
+    print("C")
+elif math >= 60:
+    print("D")
+else:
+    print("F")
